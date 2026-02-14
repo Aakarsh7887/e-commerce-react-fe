cf66886c4ff73d9eae31dc335d54d23dd2642907
Aakarsh Rai <aakarshrai49@gmail.com>
2026-02-15 02:03:36 +0530

    Added home page

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+function ProductCard({ props }) {
+  return (
+    <div className="product-card">
+      <img
+        src={props.image}
+        alt="product-image"
+        className="product-card-image"
+      />
+      <div className="product-card-content">
+        <h3 className="product-card-name">{props.title}</h3>
+        <p className="product-card-price">${props.price}</p>
+        <div className="product-card-actions">
+          <Link className="btn btn-secondary">View Details</Link>
+          <button className="btn btn-primary">Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
